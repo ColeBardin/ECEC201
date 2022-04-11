9f01527840b29329d4603a40476a464fd528237f
ColeBardin <cab572@drexel.edu>
2022-04-11 17:37:23 -0400

    fixed whitespace for chapter problem code

--- week3/lecture3/CH7P10.c
+++ week3/lecture3/CH7P10.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 int main()
 {
-  int count=0;
-  char ch;
-printf("Enter a sentence terminated by .");
-while(1)
-  {
-      scanf("%c",&ch);
-      if((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
-        count++;
- else if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
-        count++;
-    //printf("No of vowels= %d\n",count);
-    if(ch=='.') break;
-  }
+	int count=0;
+ 	char ch;
+	printf("Enter a sentence terminated by .");
+
+	while(1)
+  	{
+		scanf("%c",&ch);
+      	if((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
+        	count++;
+ 		else if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+        	count++;
+    	//printf("No of vowels= %d\n",count);
+    	if(ch=='.') break;
+	}
   
-  printf("No of vowels= %d\n",count);
-return(0);
-  }
+  	printf("No of vowels= %d\n",count);
+	return(0);
+}
--- week3/lecture3/CH9P6.c
+++ week3/lecture3/CH9P6.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 int poly(int x)
 {
-  int value;
-  value=(((((3*x+2)*x-5)*x-1)*x+7)*x-6);
-  return(value);
+	int value;
+	value=(((((3*x+2)*x-5)*x-1)*x+7)*x-6);
+	return(value);
 }
+
 int main()
 {
-  int x,value;
-  printf("Enter x:");
-  scanf("%d",&x);
-  value=poly(x);
-  printf("Value of polynomial is = %d\n",value);
-  return(0);
+	int x,value;
+	printf("Enter x:");
+	scanf("%d",&x);
+	value=poly(x);
+	printf("Value of polynomial is = %d\n",value);
+	return(0);
 }
