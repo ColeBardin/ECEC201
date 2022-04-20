537dbf26288d3a09d5e79600add4b418a43944bc
ColeBardin <cab572@drexel.edu>
2022-04-20 11:00:18 -0400

    added hw4 files

---
+++ week4/hw4-cab572/Q1.c
@@ -0,0 +1,30 @@
+/* Homework 3: Question 1
+   Compile with: gcc -std=c89 -Wall -Wpedantic -Werror -fno-common -o Q1 Q1.c
+   Then run your compiled program with: ./Q1
+*/
+
+#include <stdio.h>
+
+void swap (int *a, int *b)
+{
+  /* Your code goes here */
+}
+
+
+int main ()
+{
+  int x = 27;
+  int y = 99;
+  
+  printf("Before swap:\n");
+  printf("  x: %d\n", x);
+  printf("  y: %d\n", y);
+  
+  swap(&x, &y);
+  
+  printf("After swap:\n");
+  printf("  x: %d\n", x);
+  printf("  y: %d\n", y);
+  
+  return 0;
+}
---
+++ week4/hw4-cab572/Q2.c
@@ -0,0 +1,31 @@
+/* Homework 3: Question 2
+   Compile with: gcc -std=c89 -Wall -Wpedantic -Werror -fno-common -o Q2 Q2.c
+   Then run your compiled program with: ./Q2
+*/
+
+#include <stdio.h>
+
+int *find_largest (int *a, int n)
+{
+  /* Your code goes here */
+}
+
+
+int main ()
+{
+  int *largest;
+  
+  int numbers[] = {10,  1, 54,  8, 78, 13,  4,
+                   32, 72, 13, 43, 66, 42, 21,
+                   41, 87, 23, 11, 52,  7,  2,
+                    3, 33,  5,  9, 67, 12, 17};
+
+  int n = sizeof(numbers)/sizeof(int);
+  
+  largest = find_largest(numbers, n);
+  
+  printf("Of the %d numbers passed,\n", n);
+  printf("%d was the largest\n", *largest);
+  
+  return 0;
+}
