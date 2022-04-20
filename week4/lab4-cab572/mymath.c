ffe124b3fc928d35c86691fe2c8fa1e41a93de8b
ColeBardin <cab572@drexel.edu>
2022-04-20 14:35:09 -0400

    added header and c files for lab4. finished code as well

---
+++ week4/lab4-cab572/mymath.c
@@ -0,0 +1,19 @@
+void compute_fibs (unsigned long int *fibs, unsigned int *N){
+
+	/* create current_fib, next_fib vars as unsigned long ints */
+	unsigned long int current_fib = 0;
+	unsigned long int next_fib = 1;
+	/* Make unsigned int for index variable */
+	unsigned int index;
+
+	/* Iterate N times */
+	for (index=0; index<*N; index++){
+		/* Store current fib in current index of fibs array */
+		fibs[index] = current_fib;
+		
+		/* Set next fib to itself plus next */
+		next_fib += current_fib; 
+		/* Set current fib to be next fib minus current fib (last next fib) */
+		current_fib = next_fib - current_fib;
+	}
+}
---
+++ week4/lab4-cab572/mymath.h
@@ -0,0 +1,3 @@
+#include "mymath.c"
+
+void compute_fibs (unsigned long int *fibs, unsigned int *N);
