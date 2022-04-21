964e66acc6124069abe50f5234593125de21baf1
ColeBardin <cab572@drexel.edu>
2022-04-21 17:20:38 -0400

    redid lab with limit header and optimized code

--- week4/lab4-cab572/mymath.c
+++ week4/lab4-cab572/mymath.c
@@ -1,26 +1,27 @@
+#include <limits.h>
+
+#include "mymath.h"
+
 void compute_fibs (unsigned long int *fibs, unsigned int *N){
-	/* create current_fib, next_fib vars as unsigned long ints */
-	unsigned long int current_fib = 0;
-	unsigned long int next_fib = 1;
 	/* Make unsigned int for index variable */
 	unsigned int index;
+	
+	/* Set first and second elements to create fib sequence */
+	fibs[0] = 0;
+	fibs[1] = 1;
 
 	/* Iterate N times */
-	for (index=0; index<*N; index++){
-		/* Store current fib in current index of fibs array */
-		fibs[index] = current_fib;
-		
+	for (index=2; index<*N; index++){
+
 		/* If the next fib is less than the current one, next fib has overflown */
-		if ( current_fib > next_fib ) {
+		if ( fibs[index-1] > ULONG_MAX - fibs[index-2] ) {		
 			/* Set N to be current number of fibs printed correctly and return*/
 			*N = index + 1;
 			return;
 		}
-
-		/* Set next fib to itself plus next */
-		next_fib += current_fib; 
-		/* Set current fib to be next fib minus current fib (last next fib) */
-		current_fib = next_fib - current_fib;
+		
+		/* Calculate next fib element */
+		fibs[index] = fibs[index-1] + fibs[index-2];		
 	}
 }
 
--- week4/lab4-cab572/mymath.h
+++ week4/lab4-cab572/mymath.h
@@ -1,3 +1,1 @@
-#include "mymath.c" /* Include the func defitions file */
-
 void compute_fibs (unsigned long int *fibs, unsigned int *N); /* Func prototype */
