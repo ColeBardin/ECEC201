1691f6cf383d665a0b2b5b78b2c64041898e265b
ColeBardin <cab572@drexel.edu>
2022-04-21 17:25:50 -0400

    fixed comment

--- week4/lab4-cab572/mymath.c
+++ week4/lab4-cab572/mymath.c
@@ -10,17 +10,17 @@ void compute_fibs (unsigned long int *fibs, unsigned int *N){
 	fibs[0] = 0;
 	fibs[1] = 1;
 
-	/* Iterate N times */
+	/* Iterate from 2 to N */
 	for (index=2; index<*N; index++){
 
-		/* If the next fib is less than the current one, next fib has overflown */
+		/* If next value will overflow max value for unsigned long */
 		if ( fibs[index-1] > ULONG_MAX - fibs[index-2] ) {		
-			/* Set N to be current number of fibs printed correctly and return*/
+			/* Set N to be current number of fibs printed correctly and return */
 			*N = index + 1;
 			return;
 		}
 		
-		/* Calculate next fib element */
+		/* Set next fib element */
 		fibs[index] = fibs[index-1] + fibs[index-2];		
 	}
 }
