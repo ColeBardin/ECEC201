27603c4a72cda7ec877ca7defee9ea401e43e40f
ColeBardin <cab572@drexel.edu>
2022-05-02 21:42:50 -0400

    finished problem 1

--- week6/hw5-cab572/Q1.c
+++ week6/hw5-cab572/Q1.c
@@ -15,10 +15,11 @@ int g(int val)
 /* Your sum function goes here */
 int sum(int (*g)(int i), int i, int j) {
 	int total = 0;
-	for (;i<=j;i++){
+	if (i==j){
+		total += 2*g(i);
+	}
+	while (++i<=j){
 		total += g(i);
-		if (i==j)
-			total += g(i);
 	}
 	return total;
 }
@@ -26,7 +27,10 @@ int sum(int (*g)(int i), int i, int j) {
 
 int main()
 {
-  printf("Result: %d\n", sum(g, 4, 3));
+	int i,j;
+	if (scanf("%d,%d",&i,&j) != 2)
+		return -1;
+	printf("Result: %d\n", sum(g, i, j));
 
-  return 0;
+	return 0;
 }
