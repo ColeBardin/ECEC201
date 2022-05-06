3666a3ac2dc78eeacccfe40dc28e80ad53b71c65
ColeBardin <cab572@drexel.edu>
2022-05-06 19:21:28 +0000

    Finished Q1 of hw7

--- week7/hw6-cab572/Q1.c
+++ week7/hw6-cab572/Q1.c
@@ -3,20 +3,38 @@
    Then run your compiled program with: ./Q1
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Your code goes here */
+char *duplicate(const char *src){
+	int i;
+	char *dst;
+
+	/* Get length of src string manually if string.h shouldn't be included. else just use strlen()*/
+	for(i=0;src[i]!='\0';i++){};
+	/* malloc length of src struing plus 1 for null char */
+	dst = malloc(i+1);
+	
+	i=0;
+	/* Copy src string into memory allocated for dst string */
+	while ((dst[i] = src[i]) != '\0'){
+		i++;
+	}
+	/* Return heap address for dst string */
+	return dst;
+}
 
 int main()
 {
-  char *dst;
+	char *dst;
 
-  char *test = "Simplicity is the ultimate sophistication.";
+	char *test = "Simplicity is the ultimate sophistication.";
 
-  dst = duplicate(test);
+	dst = duplicate(test);
+	
+ 	printf("%s", dst);
 
-  printf("%s", dst);
+	free(dst);
 
-  free(dst);
-
-  return 0;
+	return 0;
 }
