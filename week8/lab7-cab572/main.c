fd7fcee7a946e16d5a8ea65e9be9d41d673054af
ColeBardin <cab572@drexel.edu>
2022-05-13 12:25:59 -0400

    commented lab7

--- week8/lab7-cab572/main.c
+++ week8/lab7-cab572/main.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define BUFF_SIZE	(32)
 
 int main()
 {
-	/* Your code goes here! */
 	FILE *fp_l, *fp_o;
 	char fn_l[] = "lorum.txt";
 	char fn_o[] = "count.txt";
 	int line, count, c;
 
+	/* Open lorum.txt in Read mode to fp_l file pointer */
 	fp_l = fopen(fn_l, "r");
+	/* Check if the file pointer is void */
 	if (!fp_l) {
+		/* If so, print error to stderr and exit */
 		fprintf(stderr, "Failed to open %s\n", fn_l);
-		return EXIT_FAILURE;
+		return -1;
 	}
 
+	/* Open count.txt in Write mode to fp_o file pointer */
 	fp_o = fopen(fn_o, "w");
+	/* Check if the file pointer is void */
 	if (!fp_o) {
+		/* If so, print error to stderr and exit */
 		fprintf(stderr, "Failed to open %s\n", fn_o);
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	line = 1;
 	count = 0;
+	/* Get each character of the file until reaching EOF */
 	while ( (c = fgetc(fp_l)) != EOF ) {
+		/* If the character pulled is a newline */
 		if (c == '\n') {
 			printf("%d: %d\n", line, count);
+			/* Write the current line and char data to count.txt */
 			fprintf(fp_o, "%d: %d\n", line, count);
+			/* Increment line number and reset character count */
 			line++;
 			count = 0;
 		} else {
+			/* Increment character count if char is anything other than newline */
 			count++;
 		}
-	}	
+	}
 	
+	/* After parsing entire lorum.txt file, close both file pointers */
 	fclose(fp_o);
 	fclose(fp_l);
 	return 0;
