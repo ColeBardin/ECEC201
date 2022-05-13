32d0c82c4c6cd664a2bdc8e5819d729f90780b0a
ColeBardin <cab572@drexel.edu>
2022-05-12 22:19:06 -0400

    added week 8 hw and lab files

---
+++ week8/hw7-cab572/README.md
@@ -0,0 +1,11 @@
+ECE-C201 Homework 7
+===================
+
+Provide your solution in the file named `main.c`.
+
+Name your zip file: `HW7-abc123.zip`
+
+Use your Drexel username instead of `abc123`! :) 
+
+
+(Please see Lab 1 for details regarding how to submit an assignment)
---
+++ week8/hw7-cab572/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+/* prints a line of 10 bytes (max) in hex with a space between each byte */
+void print_bytes_hex(unsigned char *b, unsigned int nbytes)
+{
+  /* Your code goes here */
+}
+
+
+/* prints a line of 10 bytes (max) in as characters.
+ * non-printable bytes are printed as the period character. */
+void print_bytes_char(unsigned char *b, unsigned int nbytes)
+{
+  /* Your code goes here */
+}
+
+
+int main()
+{
+  /* Your code goes here */
+
+  printf("Offset              Bytes              Characters\n");
+  printf("------  -----------------------------  ----------\n");
+
+  /* Your code goes here */
+
+  return 0;
+}
