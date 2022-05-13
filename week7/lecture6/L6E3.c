5a6569ce066a25612444d57427027dedc5575de0
ColeBardin <cab572@drexel.edu>
2022-05-12 22:16:55 -0400

    fixed horrible syntax of lecture 6 example codes

--- week7/lecture6/L6E3.c
+++ week7/lecture6/L6E3.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 int main()
 {
-        int i;
-        int *foo;
-        foo=malloc(12);
-        *foo=25;
-        *(foo+11)=5400;
-        for(i=0;i<12;i++)
-        printf("\nfoo: %d",*(foo+i));
-        free(foo);
-        return(0);
+	int i;
+	int *foo;
+	foo=malloc(12);
+	*foo=25;
+	*(foo+11)=5400;
+	for(i=0;i<12;i++)
+	printf("\nfoo: %d",*(foo+i));
+	free(foo);
+	return(0);
 }
--- week7/lecture6/L6E4.c
+++ week7/lecture6/L6E4.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-        struct stuff {
-                int val;
-                char key;
-                float pi;
-        };
+struct stuff {
+	int val;
+	char key;
+	float pi;
+};
 
-        int main()
-        {
-                struct stuff *foo;
-                foo=malloc(sizeof(*foo));
-                foo->val=1;
-                foo->key='B';
-                foo->pi=3.14f;
+int main()
+{
+	struct stuff *foo;
+	foo=malloc(sizeof(*foo));
+	foo->val=1;
+	foo->key='B';
+	foo->pi=3.14f;
 
-                printf("val: %d\n",foo->val);
-                printf("key: %c\n",foo->key);
-                printf("pi: %.2f\n",foo->pi);
-                free(foo);
-                return(0);
-        }
+	printf("val: %d\n",foo->val);
+	printf("key: %c\n",foo->key);
+	printf("pi: %.2f\n",foo->pi);
+	free(foo);
+	return(0);
+}
